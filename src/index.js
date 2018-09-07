be6dccc6d72b9a93cd167cd733484978a4636c9f
jing gong <leogong99@gmail.com>
2018-09-07 00:38:08 -0700

    add server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
- import _ from 'lodash';
- import './style.css';
- import Icon from './icon.png';
- import Data from './data.xml';
+import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
 
-function component() {
-  let element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src = Icon;
+ function component() {
+   var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-  element.appendChild(myIcon);
-  console.log(Data);
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-document.body.appendChild(component());
+    element.appendChild(btn);
+
+   return element;
+ }
+
+ let element = component(); // Store the element to re-render on print.js changes
+ document.body.appendChild(element);
+
+ if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('Accepting the updated printMe module!');
+     document.body.removeChild(element);
+     element = component(); // Re-render the "component" to update the click handler
+     document.body.appendChild(element);
+   })
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+     app: './src/index.js'
+   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-         test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: [
-           'file-loader'
-         ]
-      },
-      {
-         test: /\.(csv|tsv)$/,
-         use: [
-           'csv-loader'
-         ]
-       },
-       {
-         test: /\.xml$/,
-         use: [
-           'xml-loader'
-         ]
-       }
+   rules: [
+     {
+       test: /\.css$/,
+       use: ['style-loader', 'css-loader']
+     }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ManifestPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
